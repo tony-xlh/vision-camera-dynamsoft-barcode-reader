a68cf944dac09400412470c7824e21ca3a577414
xulihang <xulihanghai@163.com>
2022-01-26 18:57:55 -0800

    rename index.tsx to index.js; correctly decode from uiimage

--- VisionCameraDBRPlugin.swift
+++ VisionCameraDBRPlugin.swift
@@ -10,21 +10,36 @@ import DynamsoftBarcodeReader
 
 @objc(VisionCameraDBRPlugin)
 public class VisionCameraDBRPlugin: NSObject, FrameProcessorPluginBase {
-    static var barcodeReader:DynamsoftBarcodeReader! = nil
+    private static var barcodeReader:DynamsoftBarcodeReader! = nil
+    private static let context = CIContext(options: nil)
     @objc
     public static func callback(_ frame: Frame!, withArgs args: [Any]!) -> Any! {
         if barcodeReader == nil {
             configurationDBR(withArgs:args)
         }
-        let image:UIImage = sampleBufferToImage(sampleBuffer: frame.buffer)
-        
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
+          print("Failed to get CVPixelBuffer!")
+          return nil
+        }
+        let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else {
+          print("Failed to create CGImage!")
+          return nil
+        }
+        var returned_results: [Any] = []
+        let image = UIImage(cgImage: cgImage)
         // code goes here
         let results = try? barcodeReader.decode(image, withTemplate: "")
         let count = results?.count ?? 0
         if count > 0 {
+            for index in 0..<count {
+                let tr = results![index]
+                returned_results.append(wrapResult(result: tr))
+            }
             print("Found barcodes")
         }
-        return []
+        return returned_results
     }
     
     static func configurationDBR(withArgs args: [Any]!) {
@@ -38,30 +53,25 @@ public class VisionCameraDBRPlugin: NSObject, FrameProcessorPluginBase {
            barcodeReader = DynamsoftBarcodeReader(licenseFromDLS: dls, verificationDelegate: self)
         }
      }
-    // CMSampleBuffer -> UIImage
-    static func sampleBufferToImage(sampleBuffer: CMSampleBuffer) -> UIImage {
-        let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)! as CVPixelBuffer
-
-        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
-
-        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
+    
+    static func wrapResult(result: iTextResult) -> Any {
+        var map: [String: Any] = [:]
         
-        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
-
-        let width = CVPixelBufferGetWidth(imageBuffer)
-        let height = CVPixelBufferGetHeight(imageBuffer)
+        map["barcodeText"] = result.barcodeText
+        map["barcodeFormat"] = result.barcodeFormatString
         
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let points = result.localizationResult?.resultPoints as! [CGPoint]
+        map["x1"] = points[0].x
+        map["x2"] = points[1].x
+        map["x3"] = points[2].x
+        map["x4"] = points[3].x
+        map["y1"] = points[0].y
+        map["y2"] = points[1].y
+        map["y3"] = points[2].y
+        map["y4"] = points[3].y
         
-        let context = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: 8,
-                                bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
-
-        let quartzImage:CGImage = context!.makeImage()!
-
-        CVPixelBufferUnlockBaseAddress(imageBuffer,CVPixelBufferLockFlags(rawValue: 0))
-        
-        let image = UIImage(cgImage: quartzImage)
-        return image
+        return map
     }
+
 
 }
