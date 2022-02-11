a196c704226304b99ff8b90c6d9bd494c792f185
xulihang <xulihanghai@163.com>
2022-02-11 17:36:00 +0800

    remove experimental code; fix viewBox

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,8 +8,8 @@ import { Polygon, Rect, Svg } from 'react-native-svg';
 export default function App() {
   const [hasPermission, setHasPermission] = React.useState(false);
   const [barcodeResults, setBarcodeResults] = React.useState([] as TextResult[]);
-  const [frameWidth, setFrameWidth] = React.useState(1280);
-  const [frameHeight, setFrameHeight] = React.useState(720);
+  const [frameWidth, setFrameWidth] = React.useState(720);
+  const [frameHeight, setFrameHeight] = React.useState(1280);
   const devices = useCameraDevices();
   const device = devices.back;
   
@@ -29,57 +29,12 @@ export default function App() {
   }, [])
 
   function getPointsData(lr:TextResult){
-    //let point1 = rotate(frameWidth/2,frameHeight/2,lr.x1,lr.y1,90);
-    //let point2 = rotate(frameWidth/2,frameHeight/2,lr.x2,lr.y2,90);
-    //let point3 = rotate(frameWidth/2,frameHeight/2,lr.x3,lr.y3,90);
-    //let point4 = rotate(frameWidth/2,frameHeight/2,lr.x4,lr.y4,90);
-    //let point1 = rotatePoint(lr.x1,lr.y1,90);
-    //let point2 = rotatePoint(lr.x2,lr.y2,90);
-    //let point3 = rotatePoint(lr.x3,lr.y3,90);
-    //let point4 = rotatePoint(lr.x4,lr.y4,90);
-    //let x1 = point1[0];
-    //let y1 = point1[1];
-    //let x2 = point2[0];
-    //let y2 = point2[1];
-    //let x3 = point3[0];
-    //let y3 = point3[1];
-    //let x4 = point4[0];
-    //let y4 = point4[1];
-    let x1 = lr.x1;
-    let x2 = lr.x2;
-    let x3 = lr.x3;
-    let x4 = lr.x4;
-    let y1 = lr.y1;
-    let y2 = lr.y2;
-    let y3 = lr.y3;
-    let y4 = lr.y4;
-    var pointsData = x1+","+y1 + " ";
-    pointsData = pointsData+ x2+","+y2 + " ";
-    pointsData = pointsData+ x3+","+y3 + " ";
-    pointsData = pointsData+ x4+","+y4;
-    console.log(pointsData);
+    var pointsData = lr.x1 + "," + lr.y1 + " ";
+    pointsData = pointsData+lr.x2 + "," + lr.y2 +" ";
+    pointsData = pointsData+lr.x3 + "," + lr.y3 +" ";
+    pointsData = pointsData+lr.x4 + "," + lr.y4;
     return pointsData;
   }
-
-  function rotate(cx, cy, x, y, angle) {
-    var radians = (Math.PI / 180) * angle,
-        cos = Math.cos(radians),
-        sin = Math.sin(radians),
-        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
-        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
-        nx = Math.floor(nx);
-        ny = Math.floor(ny);
-    return [nx, ny];
-  }
-
-  function rotatePoint(x,y,angle) { 
-    var a = angle * Math.PI / 180.0;
-    var cosa = Math.cos(a);
-    var sina = Math.sin(a);
-    var nx = x * cosa - y * sina;
-    var ny = x * sina + y * cosa;
-    return [nx,ny];
- }
 
   function getViewBox(){
     let viewBox = null;
@@ -87,7 +42,7 @@ export default function App() {
       console.log("Has rotation");
       viewBox = "0 0 "+frameHeight+" "+frameWidth;
     }else {
-      viewBox = "0 0 "+frameHeight+" "+frameWidth;
+      viewBox = "0 0 "+frameWidth+" "+frameHeight;
     }    
     console.log("viewBox"+viewBox);
     return viewBox;
