dcbaf4c1f23331f2d70f1ea881904bbc37dcb1c7
xulihang <xulihanghai@163.com>
2022-02-14 00:54:40 -0800

    iOS: runtime settings can be modified realtime

--- ios/VisionCameraDBRPlugin.swift
+++ ios/VisionCameraDBRPlugin.swift
@@ -11,12 +11,16 @@ import DynamsoftBarcodeReader
 @objc(VisionCameraDBRPlugin)
 public class VisionCameraDBRPlugin: NSObject, FrameProcessorPluginBase {
     private static var barcodeReader:DynamsoftBarcodeReader! = nil
+    private static var mTemplate:String! = nil
     private static let context = CIContext(options: nil)
     @objc
     public static func callback(_ frame: Frame!, withArgs args: [Any]!) -> Any! {
+        let config = getConfig(withArgs: args)
         if barcodeReader == nil {
-            configurationDBR(withArgs:args)
+            configurationDBR(config: config)
         }
+        
+        
         guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
           print("Failed to get CVPixelBuffer!")
           return nil
@@ -42,16 +46,12 @@ public class VisionCameraDBRPlugin: NSObject, FrameProcessorPluginBase {
         return returned_results
     }
     
-    static func configurationDBR(withArgs args: [Any]!) {
+    static func configurationDBR(config: [String:String]!) {
         var license = "";
         var organizationID = "200001";
-        var template = "";
-        if args.count>0 {
-            let config = args[0] as? [String: String]
-            license = config?["license"] ?? ""
-            organizationID = config?["organizationID"] ?? "200001"
-            template = config?["template"] ?? ""
-        }
+
+        license = config?["license"] ?? ""
+        organizationID = config?["organizationID"] ?? "200001"
         
 
         let dls = iDMDLSConnectionParameters()
@@ -62,11 +62,44 @@ public class VisionCameraDBRPlugin: NSObject, FrameProcessorPluginBase {
            barcodeReader = DynamsoftBarcodeReader(licenseFromDLS: dls, verificationDelegate: self)
         }
         
-        if template != "" {
-            var error: NSError? = NSError()
-            barcodeReader.initRuntimeSettings(with: template, conflictMode: EnumConflictMode.overwrite, error: &error)
-        }
+
      }
+    
+    static func getConfig(withArgs args: [Any]!) -> [String:String]!{
+        if args.count>0 {
+            let config = args[0] as? [String: String]
+            return config
+        }
+        return nil
+    }
+    
+    static func updateRuntimeSettingsWithTemplate(config:[String:String]!){
+        let template = config?["template"] ?? ""
+        var shouldUpdate = false
+        
+        if template != "" {
+            if mTemplate == nil {
+                shouldUpdate = true
+            } else {
+                if mTemplate != template {
+                    shouldUpdate = true
+                }
+            }
+            
+            if shouldUpdate {
+                var error: NSError? = NSError()
+                barcodeReader.initRuntimeSettings(with: template, conflictMode: EnumConflictMode.overwrite, error: &error)
+                mTemplate = template
+            }
+            
+        } else {
+            if mTemplate != nil {
+                var error: NSError? = NSError()
+                barcodeReader.resetRuntimeSettings(&error)
+                mTemplate = nil
+            }
+        }
+    }
     
     static func wrapResult(result: iTextResult) -> Any {
         var map: [String: Any] = [:]
