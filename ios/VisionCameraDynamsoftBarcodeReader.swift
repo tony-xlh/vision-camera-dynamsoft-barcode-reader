cf90b9a9c426322d78fefc48ed843b84c135d794
xulihang <xulihanghai@163.com>
2022-12-02 01:05:03 -0800

    add native modules files for iOS

---
+++ ios/VisionCameraDynamsoftBarcodeReader.swift
@@ -0,0 +1,9 @@
+//
+//  VisionCameraDynamsoftBarcodeReader.swift
+//  VisionCameraDynamsoftBarcodeReader
+//
+//  Created by xulihang on 2022/12/2.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+import Foundation
