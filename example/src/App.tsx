e19c219dbe8a7f603a8636b5f6501d4691c6f621
xulihang <xulihanghai@163.com>
2023-09-06 11:22:50 +0800

    example: use the frame processor

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import { Camera, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
 import { StyleSheet, Text } from 'react-native';
+import { decode } from 'vision-camera-dynamsoft-barcode-reader';
 
 export default function App() {
   const [hasPermission, setHasPermission] = React.useState(false);
   const devices = useCameraDevices()
   const device = devices.back
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet'
+    const results = decode(frame,{})
+    console.log(results);
+  }, [])
   
   React.useEffect(() => {
     (async () => {
@@ -20,6 +26,7 @@ export default function App() {
       style={StyleSheet.absoluteFill}
       device={device}
       isActive={true}
+      frameProcessor={frameProcessor}
     />
   )
 }
