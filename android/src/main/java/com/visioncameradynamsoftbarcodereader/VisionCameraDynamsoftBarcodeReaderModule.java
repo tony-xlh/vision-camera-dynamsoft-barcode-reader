3f98f4a9d44895dd1bb640d7ed9248d10757bc73
xulihang <xulihanghai@163.com>
2023-09-06 10:49:57 +0800

    Android: implement the basic functions of the native module

--- android/src/main/java/com/visioncameradynamsoftbarcodereader/VisionCameraDynamsoftBarcodeReaderModule.java
+++ android/src/main/java/com/visioncameradynamsoftbarcodereader/VisionCameraDynamsoftBarcodeReaderModule.java
@@ -1,19 +1,48 @@
 package com.visioncameradynamsoftbarcodereader;
 
-import androidx.annotation.NonNull;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.dynamsoft.dbr.BarcodeReader;
+import com.dynamsoft.dbr.BarcodeReaderException;
+import com.dynamsoft.dbr.DBRLicenseVerificationListener;
+import com.dynamsoft.dbr.EnumConflictMode;
+import com.dynamsoft.dbr.TextResult;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.module.annotations.ReactModule;
 
 @ReactModule(name = VisionCameraDynamsoftBarcodeReaderModule.NAME)
 public class VisionCameraDynamsoftBarcodeReaderModule extends ReactContextBaseJavaModule {
   public static final String NAME = "VisionCameraDynamsoftBarcodeReader";
-
+  private Context mContext;
+  private BarcodeReader dbr;
   public VisionCameraDynamsoftBarcodeReaderModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    mContext = reactContext;
+    initDBR();
+  }
+  private void initDBR(){
+    try {
+      dbr = new BarcodeReader();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Context getContext(){
+    return mContext;
+  }
+  public BarcodeReader getDBR(){
+    return dbr;
   }
 
   @Override
@@ -21,12 +50,49 @@ public class VisionCameraDynamsoftBarcodeReaderModule extends ReactContextBaseJa
   public String getName() {
     return NAME;
   }
-
-
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+  public void initLicense(String license, Promise promise) {
+    BarcodeReader.initLicense(license, new DBRLicenseVerificationListener() {
+      @Override
+      public void DBRLicenseVerificationCallback(boolean isSuccessful, Exception e) {
+        if (!isSuccessful) {
+          e.printStackTrace();
+          promise.reject("DBR",e.getMessage());
+        }else {
+          promise.resolve(true);
+        }
+      }
+    });
+  }
+
+  @ReactMethod
+  public void initRuntimeSettingsFromString(String template, Promise promise) {
+    try {
+      dbr.initRuntimeSettingsWithString(template, EnumConflictMode.CM_OVERWRITE);
+      promise.resolve(true);
+    } catch (BarcodeReaderException e) {
+      e.printStackTrace();
+      promise.reject("DBR",e.getMessage());
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @ReactMethod
+  public void decodeBase64(String base64, Promise promise) {
+    try {
+      Bitmap bitmap = BitmapUtils.base642Bitmap(base64);
+      TextResult[] results = dbr.decodeBufferedImage(bitmap);
+      WritableNativeArray array = new WritableNativeArray();
+      if (results != null) {
+        for (int i = 0; i < results.length; i++) {
+          Log.d("DBR",results[i].barcodeText);
+          array.pushMap(Utils.wrapResults(results[i], null, false, true));
+        }
+      }
+      promise.resolve(array);
+    } catch (BarcodeReaderException e) {
+      e.printStackTrace();
+      promise.reject("DBR",e.getMessage());
+    }
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,45 @@ const VisionCameraDynamsoftBarcodeReader = NativeModules.VisionCameraDynamsoftBa
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return VisionCameraDynamsoftBarcodeReader.multiply(a, b);
+
+export interface TextResult{
+  barcodeText:string;
+  barcodeFormat:string;
+  barcodeBytesBase64:string;
+  x1:number;
+  x2:number;
+  x3:number;
+  x4:number;
+  y1:number;
+  y2:number;
+  y3:number;
+  y4:number;
+}
+
+export interface DBRConfig{
+  template?:string;
+  license?:string;
+  isFront?:boolean;
+  rotateImage?:boolean;
+}
+
+/**
+ * Init the license of Dynamsoft Barcode Reader
+ */
+  export function initLicense(license:string): Promise<boolean> {
+  return VisionCameraDynamsoftBarcodeReader.initLicense(license);
+}
+
+/**
+ * Init the runtime settings from a JSON template
+ */
+export function initRuntimeSettingsFromString(template:string): Promise<boolean> {
+  return VisionCameraDynamsoftBarcodeReader.initRuntimeSettingsFromString(template);
+}
+
+/**
+ * Detect barcodes from base64
+ */
+  export function decodeBase64(base64:string): Promise<TextResult[]> {
+  return VisionCameraDynamsoftBarcodeReader.decodeBase64(base64);
 }
