8b197668dd0b57381f49d1e30b78867341565638
xulihang <xulihanghai@163.com>
2023-09-06 16:28:16 +0800

    example: display barcode text

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,15 +2,20 @@ import * as React from 'react';
 import { Camera, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
 import { StyleSheet, Text } from 'react-native';
 import { decode } from 'vision-camera-dynamsoft-barcode-reader';
+import { Worklets } from 'react-native-worklets-core';
 
 export default function App() {
   const [hasPermission, setHasPermission] = React.useState(false);
+  const [barcodeText,setBarcodeText] = React.useState("");
   const devices = useCameraDevices()
   const device = devices.back
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
-    const results = decode(frame,{})
+    const results:any = decode(frame,{})
     console.log(results);
+    if (results.length>0) {
+      Worklets.createRunInJsFn(setBarcodeText)(results[0].barcodeText);
+    }
   }, [])
   
   React.useEffect(() => {
@@ -22,11 +27,14 @@ export default function App() {
   if (hasPermission == false) return <Text>No camera permission</Text>
   if (device == null) return <Text>Loading...</Text>
   return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={true}
-      frameProcessor={frameProcessor}
-    />
+    <>
+      <Text>{{ barcodeText }}</Text>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+        frameProcessor={frameProcessor}
+      ></Camera>
+    </>
   )
 }
