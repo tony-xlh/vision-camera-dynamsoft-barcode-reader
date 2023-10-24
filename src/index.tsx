4501828433e0e8398727994a131dabe5e1a25072
xulihang <xulihanghai@163.com>
2023-10-24 10:40:02 +0800

    update the plugin's JavaScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import { NativeModules, Platform } from 'react-native';
+import {VisionCameraProxy, type Frame} from 'react-native-vision-camera';
+
+const plugin = VisionCameraProxy.initFrameProcessorPlugin('decode')
 
 const LINKING_ERROR =
   `The package 'vision-camera-dynamsoft-barcode-reader' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,6 +20,66 @@ const VisionCameraDynamsoftBarcodeReader = NativeModules.VisionCameraDynamsoftBa
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return VisionCameraDynamsoftBarcodeReader.multiply(a, b);
+/**
+ * Detect barcodes from the camera preview
+ */
+export function decode(frame: Frame,config:DBRConfig) {
+  'worklet'
+  if (plugin == null) throw new Error('Failed to load Frame Processor Plugin "decode"!')
+  return plugin.call(frame,convertDBRConfig(config));
+}
+
+function convertDBRConfig(config:DBRConfig):Record<string,any>{
+  let record:Record<string,any> = {};
+  if (config.isFront) {
+    record["isFront"] = config.isFront;
+  }
+  if (config.rotateImage) {
+    record["rotateImage"] = config.rotateImage;
+  }
+  if (config.template) {
+    record["template"] = config.template;
+  }
+  return record;
+}
+
+export interface TextResult{
+  barcodeText:string;
+  barcodeFormat:string;
+  barcodeBytesBase64:string;
+  x1:number;
+  x2:number;
+  x3:number;
+  x4:number;
+  y1:number;
+  y2:number;
+  y3:number;
+  y4:number;
+}
+
+export interface DBRConfig{
+  template?:string;
+  isFront?:boolean;
+  rotateImage?:boolean;
+}
+
+/**
+ * Init the license of Dynamsoft Barcode Reader
+ */
+  export function initLicense(license:string): Promise<boolean> {
+  return VisionCameraDynamsoftBarcodeReader.initLicense(license);
+}
+
+/**
+ * Init the runtime settings from a JSON template
+ */
+export function initRuntimeSettingsFromString(template:string): Promise<boolean> {
+  return VisionCameraDynamsoftBarcodeReader.initRuntimeSettingsFromString(template);
+}
+
+/**
+ * Detect barcodes from base64
+ */
+export function decodeBase64(base64:string): Promise<TextResult[]> {
+  return VisionCameraDynamsoftBarcodeReader.decodeBase64(base64);
 }
