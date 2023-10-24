a9b2a30b2fbb89d3ba852dca6bc9e7c433750429
xulihang <xulihanghai@163.com>
2023-10-24 13:45:39 +0800

    update the example to add both static image decoding and live decoding functions

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,56 +1,123 @@
 import * as React from 'react';
+import { Button, SafeAreaView, StyleSheet, Switch, Text, View } from 'react-native';
+import type { TextResult } from 'vision-camera-dynamsoft-barcode-reader';
+import * as DBR from 'vision-camera-dynamsoft-barcode-reader';
+import {launchImageLibrary, type ImageLibraryOptions} from 'react-native-image-picker';
+import BarcodeScanner from './components/BarcodeScanner';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { Camera, useCameraDevice, useFrameProcessor } from 'react-native-vision-camera';
-import { useSharedValue, Worklets } from 'react-native-worklets-core';
-import { decode, type TextResult } from 'vision-camera-dynamsoft-barcode-reader';
+const Separator = () => (
+  <View style={styles.separator} />
+);
 
 export default function App() {
-  const [hasPermission, setHasPermission] = React.useState(false);
-  const device = useCameraDevice('back');
-  const [barcodeResults,setBarcodeResults] = React.useState<TextResult[]>([]);
-  //const setResultsJS = Worklets.createRunInJsFn(setBarcodeResults);
-  const fps = 1;
-
+  const [useCamera,setUseCamera] = React.useState(false);
+  const [continuous, setContinuous] = React.useState(false);
+  const [barcodeResults, setBarcodeResults] = React.useState([] as TextResult[]);
+  const toggleSwitch = () => setContinuous(previousState => !previousState);
+  
   React.useEffect(() => {
     (async () => {
-      const status = await Camera.requestCameraPermission();
-      setHasPermission(status === 'granted');
+      const result = await DBR.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==");
+      console.log(result);
     })();
   }, []);
-  const frameProcessor = useFrameProcessor(frame => {
-    'worklet';
-    const results = decode(frame);
-    console.log(results);
-    //setBarcodeResults(results)
-  }, []);
-  
-  if (hasPermission == false) return <Text>No camera permission</Text>
-  if (device == null) return <Text>No Camera</Text>
+
+  const onScanned = (results:TextResult[]) => {
+    setBarcodeResults(results);
+    if (results.length>0 && !continuous) {
+      setUseCamera(false);
+    }
+  }
+
+  const decodeFromAlbum = async () => {
+    let options: ImageLibraryOptions = {
+      mediaType: 'photo',
+      includeBase64: true,
+    }
+    let response = await launchImageLibrary(options);
+    if (response && response.assets) {
+      if (response.assets[0]!.base64) {
+        console.log(response.assets[0]!.base64);
+        let results = await DBR.decodeBase64(response.assets[0]!.base64);
+        setBarcodeResults(results);
+      }
+    }
+  }
+
   return (
-    <>
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={true}
-        frameProcessor={frameProcessor}
-        fps={fps}
-      />
-      <Text></Text>
-    </>
-    
-  )
+    <SafeAreaView style={styles.container}>
+      {useCamera && (
+        <>
+          <BarcodeScanner onScanned={onScanned}></BarcodeScanner>
+          <View
+            style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'flex-end', alignItems: 'center'}}
+          >
+            <Button
+              title="Close"
+              onPress={() => setUseCamera(false)}
+            />
+          </View>
+        </>
+        
+      )}
+      {!useCamera &&(
+          <View style={{alignItems:"center"}}>
+            <Text style={styles.title}>
+              Dynamsoft Barcode Reader Demo
+            </Text>
+            <Button
+              title="Read Barcodes from the Camera"
+              onPress={() => setUseCamera(true)}
+            />
+            <Separator />
+            <View style={styles.switchView}>
+              <Text style={{alignSelf: 'center'}}>
+                Continues Scan
+              </Text>
+              <Switch 
+                style={{alignSelf: 'center'}}
+                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                thumbColor={continuous ? "#f5dd4b" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitch}
+                value={continuous}
+              />
+            </View>
+            <Separator />
+            <Button
+              title="Read Barcodes from the Album"
+              onPress={() => decodeFromAlbum()}
+            />
+            <Separator />
+            {barcodeResults.map((barcode, idx) => (
+              <Text key={idx}>
+                {barcode.barcodeFormat+": "+barcode.barcodeText}
+              </Text>
+            ))}
+          </View>
+      )}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  separator: {
+    marginVertical: 4,
+  },
+  switchView: {
+    alignItems: 'center',
+    flexDirection: "row",
+  },
+  barcodeText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
---
+++ example/src/components/BarcodeScanner.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { Camera, useCameraDevice, useFrameProcessor } from 'react-native-vision-camera';
+import { type DBRConfig, type TextResult } from 'vision-camera-dynamsoft-barcode-reader';
+
+interface props {
+  onScanned?: (result:TextResult[]) => void;
+}
+
+const BarcodeScanner: React.FC<props> = (props: props) => {
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [barcodeResults, setBarcodeResults] = React.useState([] as TextResult[]);
+  const device = useCameraDevice('back');
+  
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet'
+    const config:DBRConfig = {};
+  }, [])
+
+
+  React.useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    if (props.onScanned) {
+      props.onScanned(barcodeResults);
+    }
+  }, [barcodeResults]);
+
+  return (
+      <>
+        {device != null &&
+        hasPermission && (
+        <>
+            <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={true}
+            frameProcessor={frameProcessor}
+            />
+            {barcodeResults.map((barcode, idx) => (
+            <Text key={idx} style={styles.barcodeText}>
+                {barcode.barcodeFormat +": "+ barcode.barcodeText}
+            </Text>
+            ))}
+        </>)}
+      </>
+  );
+}
+
+export default BarcodeScanner;
+
+const styles = StyleSheet.create({
+  barcodeText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
