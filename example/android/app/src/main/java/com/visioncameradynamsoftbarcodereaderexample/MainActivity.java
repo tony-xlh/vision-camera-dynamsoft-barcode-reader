39f35992a0eecd8e8809e64e681f51165e7be6d1
xulihang <xulihanghai@163.com>
2023-11-20 15:39:53 +0800

    example: rename the android example's name

--- example/android/app/src/main/java/com/visioncameradynamsoftbarcodereaderexample/MainActivity.java
+++ example/android/app/src/main/java/com/visioncameradynamsoftbarcodereaderexample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.visioncameraocrexample;
+package com.visioncameradynamsoftbarcodereaderexample;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- example/android/app/src/main/java/com/visioncameradynamsoftbarcodereaderexample/MainApplication.java
+++ example/android/app/src/main/java/com/visioncameradynamsoftbarcodereaderexample/MainApplication.java
@@ -1,4 +1,4 @@
-package com.visioncameraocrexample;
+package com.visioncameradynamsoftbarcodereaderexample;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
