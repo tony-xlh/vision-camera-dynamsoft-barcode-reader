f7940bc59410ad34d745cd58bfd44af297e234a8
xulihang <xulihanghai@163.com>
2024-02-19 15:06:18 +0800

    example: use runAsync

--- example/src/components/BarcodeScanner.tsx
+++ example/src/components/BarcodeScanner.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, Text } from 'react-native';
-import { Camera, useCameraDevice, useFrameProcessor } from 'react-native-vision-camera';
+import { Camera, runAsync, runAtTargetFps, useCameraDevice, useFrameProcessor } from 'react-native-vision-camera';
 import { decode, type DBRConfig, type TextResult } from 'vision-camera-dynamsoft-barcode-reader';
 import { Worklets} from 'react-native-worklets-core';
 
@@ -9,22 +9,25 @@ interface props {
 }
 
 const BarcodeScanner: React.FC<props> = (props: props) => {
+  const device = useCameraDevice('back');
   const [hasPermission, setHasPermission] = React.useState(false);
   const [results, setResults] = React.useState([] as TextResult[]);
-  const device = useCameraDevice('back');
   const setResultsJS = Worklets.createRunInJsFn(setResults);
 
   const frameProcessor = useFrameProcessor(frame => {
     'worklet'
-    const config:DBRConfig = {
-      rotateImage:false,
-      template:"{\"ImageParameter\":{\"BarcodeFormatIds\":[\"BF_ALL\"],\"Description\":\"\",\"Name\":\"Settings\"},\"Version\":\"3.0\"}"
-    };
-    const results = decode(frame,config);
-    console.log(results);
-    if (results) {
-      setResultsJS(results);
-    }
+    runAsync(frame, () => {
+      'worklet'
+      const config:DBRConfig = {
+        rotateImage:false,
+        template:"{\"ImageParameter\":{\"BarcodeFormatIds\":[\"BF_ALL\"],\"Description\":\"\",\"Name\":\"Settings\"},\"Version\":\"3.0\"}"
+      };
+      const results = decode(frame,config);
+      console.log(results);
+      if (results) {
+        setResultsJS(results);
+      }
+    })
   }, [])
 
   React.useEffect(() => {
