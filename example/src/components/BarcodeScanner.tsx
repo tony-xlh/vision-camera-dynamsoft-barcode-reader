29be4f9da4f8a588757f313c73e807af78ddc322
xulihang <xulihanghai@163.com>
2024-02-19 16:36:15 +0800

    example: specify resolution, add an isActive state and runAtTargetFPS

--- example/src/components/BarcodeScanner.tsx
+++ example/src/components/BarcodeScanner.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, Text } from 'react-native';
-import { Camera, runAsync, runAtTargetFps, useCameraDevice, useFrameProcessor } from 'react-native-vision-camera';
+import { Camera, runAsync, runAtTargetFps, useCameraDevice, useCameraFormat, useFrameProcessor } from 'react-native-vision-camera';
 import { decode, type DBRConfig, type TextResult } from 'vision-camera-dynamsoft-barcode-reader';
 import { Worklets} from 'react-native-worklets-core';
 
@@ -9,14 +9,18 @@ interface props {
 }
 
 const BarcodeScanner: React.FC<props> = (props: props) => {
-  const device = useCameraDevice('back');
   const [hasPermission, setHasPermission] = React.useState(false);
+  const [isActive, setIsActive] = React.useState(false);
   const [results, setResults] = React.useState([] as TextResult[]);
   const setResultsJS = Worklets.createRunInJsFn(setResults);
-
+  const device = useCameraDevice("back");
+  const cameraFormat = useCameraFormat(device, [
+    { videoResolution: { width: 1280, height: 720 } },
+    { fps: 60 }
+  ])
   const frameProcessor = useFrameProcessor(frame => {
     'worklet'
-    runAsync(frame, () => {
+    runAtTargetFps(3, () => {
       'worklet'
       const config:DBRConfig = {
         rotateImage:false,
@@ -34,6 +38,7 @@ const BarcodeScanner: React.FC<props> = (props: props) => {
     (async () => {
       const status = await Camera.requestCameraPermission();
       setHasPermission(status === 'granted');
+      setIsActive(true);
     })();
   }, []);
 
@@ -45,13 +50,14 @@ const BarcodeScanner: React.FC<props> = (props: props) => {
 
   return (
       <>
-        {device != null &&
+        {device &&
         hasPermission && (
         <>
             <Camera
             style={StyleSheet.absoluteFill}
             device={device}
-            isActive={true}
+            isActive={isActive}
+            format={cameraFormat}
             frameProcessor={frameProcessor}
             pixelFormat="yuv"
             />
